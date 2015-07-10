7a86438ddc5ddd72eed05a884fb36fd8d5fc1137
marscapone <samson.danziger@gmail.com>
2015-07-10 08:47:35 +0100

    fix downloader

--- ff/downloader.py
+++ ff/downloader.py
@@ -4,43 +4,36 @@ import sys, getopt
 import pdfkit
 from fiction import *
 
-root = "http://www.fanfiction.net"
+root = "https://www.fanfiction.net"
 
-def get_chapters(url=None, storyid=None):
-    if url is None:
-        if storyid is None:
-            print "Either url or storyid must be provided."
-        else:
-            url = root + "/s/%d" % storyid
-    story = Story(url)
+
+def get_chapters(story):
     chapter_gen = story.get_chapters()
     chapters = []
     for chapter in chapter_gen:
         chapters.append(chapter)
     return chapters
 
+
 def get_total_story_html(chapter_list):
     html = ""
-    for chapter in chapter_list:
-        html += chapter.raw_text
-        html += '\n'
+    for i in range(len(chapter_list)):
+        html += '<h2>Chapter %s</h2>\n' % str(i + 1)  # couldn't use %d here for some reason
+        html += chapter_list[i].raw_text
+        html += '</br>' * 10  # new chapters after a break
     return html
 
-def download_pdf(url, output):
-    if output[-4:] != ".pdf":
-        output += ".pdf"
-    chapters = get_chapters(url)
+
+def download_pdf(story, output):
+    chapters = get_chapters(story)
     html = get_total_story_html(chapters)
     pdfkit.from_string(html, output)
 
-def download_pdf_id(storyid, output):
-    url = root + "/s/%d" % storyid
-    download_pdf(url, output)
 
 def main(argv):
     url = ''
     storyid = 0
-    output = 'out.pdf'
+    output = ''
     help_string = """
     downloader.py -h
     downloader.py -u [url] -o [output]
@@ -62,14 +55,20 @@ def main(argv):
         elif opt in ("-s", "--storyid"):
             storyid = int(arg)
     if len(url) > 0:
-        print "Downloading fanfiction to %s" % output
-        download_pdf(url, output)
+        story = Story(url=url)
     elif storyid != 0:
-        print "Downloading fanfiction to %s" % output
-        download_pdf_id(storyid, output)
+        story = Story(id=storyid)
     else:
         print "url or storyid not provided"
         print help_string
+    if output == '':
+        output = "%s_by_%s" % (story.title, story.author)
+        output = output.replace(' ', '-')
+        if output[-4:] != ".pdf":  # output should be a pdf file
+            output += ".pdf"
+    print "Downloading %s to %s" % (story.title, output)
+    download_pdf(story, output)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
