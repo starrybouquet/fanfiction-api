6521885cb431d09e26b109850498e3ca25b7a0f8
Samson Danziger <samson.danziger@gmail.com>
2015-07-25 15:01:39 +0100

    fix conflicts. update readme

--- ff/__init__.py
+++ ff/__init__.py
@@ -1,10 +1,5 @@
 __author__ = 'Samson Danziger'
 
 from fiction import *
-<<<<<<< HEAD
 from downloader import *
-from authentication import FFLogin
-=======
-from downloader import download_pdf
 #from authentication import FFLogin
->>>>>>> 99df027e36d0cdaa22c1842cffb3c256657567be
